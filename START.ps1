6afade6521f604c76b73dceff6bf96def82d6483
Guga <gumart.n3z@gmail.com>
2025-02-17 22:06:52 -0300

    ae

--- START.ps1
+++ START.ps1
@@ -1,8 +1,8 @@
 # Inicia o pocketbase.exe em segundo plano
 Start-Process -FilePath "./pocketbase.exe" -ArgumentList "serve" -NoNewWindow
 
-# Roda o comando npm run dev e aguarda
-npm run start
-
 # Abre o navegador na URL localhost:3000
 Start-Process "http://localhost:3000"
+
+# Roda o comando npm run dev e aguarda
+npm run start
